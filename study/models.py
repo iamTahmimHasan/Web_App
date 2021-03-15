ae43d2b3b714a5fda545de45dc73f418044eed3a
Tahmim Hasan Sany <sanytahmim@gmail.com>
2021-03-15 17:38:44 +0600

    django Model Update

--- study/models.py
+++ study/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+#class Destination(models.Model):
+    #name = models.CharField(max_length=100)
+    #img = models.ImageField(upload_to='pics')
+    #desc = models.TextField()
+    #price = models.IntegerField()
+    #offer = models.BooleanField(default=False)
+ 
+class university (models.Model):
+        university_logo= models.ImageField(upload_to='pics')
+        university_name = models.CharField(max_length=100)
+        university_rank = models.CharField(max_length=100)
+        university_country=  models.CharField(max_length=100)
+    
