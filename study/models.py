6aab6c84672b93206f91d6e5a12a09288518a9c9
Tahmim Hasan Sany <sanytahmim@gmail.com>
2021-03-18 14:11:47 +0600

    admin registry update, meta & Verbose_name

--- study/models.py
+++ study/models.py
@@ -10,8 +10,15 @@ from django.db import models
     #offer = models.BooleanField(default=False)
  
 class University (models.Model):
-        university_logo = models.ImageField(upload_to='pics')
-        university_name = models.CharField(max_length=255)
-        university_rank = models.CharField(max_length=50)
-        university_country = models.CharField(max_length=100)
+        university_logo = models.ImageField(upload_to='pics', verbose_name="University Logo :")
+        university_name = models.CharField(max_length=255, verbose_name="Name :")
+        university_rank = models.CharField(max_length=50, verbose_name="Rank :", default="N/A")
+        university_country = models.CharField(max_length=100, verbose_name="Country :")
+
+        def __str__(self):
+            return self.university_name
+        
+        class Meta:
+            verbose_name = 'Top University'
+            verbose_name_plural = 'Top Universitys'
     
