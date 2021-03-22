6fbb44af78f13214c363521d7a3cfe0c55d991dd
Tahmim Hasan Sany <sanytahmim@gmail.com>
2021-03-22 18:23:07 +0600

    Contat page add

--- study/urls.py
+++ study/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name ='home')
+    path('', views.home, name ='home'),
+    path('base', views.base, name ='base'),
+    path('contact', views.contact, name ='contact'),
 ]
--- study/views.py
+++ study/views.py
@@ -6,3 +6,10 @@ from .models import University
 def home(request):
     hot_universitys = University.objects.all()
     return render(request,'study.html', {'hot_universitys': hot_universitys})
+
+
+def base(request):
+    return render(request, 'base.html')
+
+def contact(request):
+    return render(request, 'contact.html')
