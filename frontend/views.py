5b22a82322f1718ef1130256f741f8cd443e4592
Tahmim Hasan Sany <31439467+iamTahmimHasan@users.noreply.github.com>
2021-04-11 11:18:07 +0600

    Employment

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TravelCountry, EmploymentCountry, StudyCountry
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',{'name':'Tahmim'})
+    travels= TravelCountry.objects.all()
+    employments= EmploymentCountry.objects.all()
+    studys= StudyCountry.objects.all()
+    return render(request, 'home.html',
+    {
+        'travels':travels,
+        'employments' : employments,
+         'studys' :studys,
+    })
